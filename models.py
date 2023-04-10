eab35e391c3a90365b624ae007a69885866fd466
far <Qwerty@far.com>
2023-04-10 19:00:06 +0800

    commit 5

--- models.py
+++ models.py
@@ -7,24 +7,21 @@ class MyMNIST(nn.Module):
     def __init__(self):
         super(MyMNIST, self).__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(1, 32, 3, 1, 1),
+            nn.Conv2d(1, 16, 3, 1, 1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(16, 16, 3, 1, 1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=1),
+            nn.Conv2d(16, 32, 3, 1, 1),
             nn.ReLU(inplace=True),
             nn.Conv2d(32, 32, 3, 1, 1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2, stride=1),
-            nn.Conv2d(32, 64, 3, 1, 1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, 3, 1, 1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=1),
             nn.Flatten(),
-            nn.Linear(26*26*64, 4096),
+            nn.Linear(26*26*32, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5, inplace=False),
-            nn.Linear(4096, 1024),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.5, inplace=False),
-            nn.Linear(1024, 10)
+            nn.Linear(4096, 10)
         )
 
     def forward(self, x):
